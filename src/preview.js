ff22546eb0ce5df176a3713afa399559272691e0
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2022-06-17 22:42:32 +0300

    Fix typo

--- src/preview.js
+++ src/preview.js
@@ -37,7 +37,7 @@ export default function Preview( { countryCode, relatedPosts } ) {
 				<h3 className="xwp-country-card__related-posts__heading">
 					{ hasRelatedPosts
 						? sprintf(
-								/* translators: numer related posts. */
+								/* translators: number of related posts. */
 								__(
 									'There are %d related posts:',
 									'xwp-country-card'
